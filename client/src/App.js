f6e0e0d94cfa79773125c32b0f23e6dd104e2756
kalyanimannam@12 <kalyanimannam274@gmail.com>
2024-01-09 17:06:50 +0530

    added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Login from './components/Login';
 import Signup from './components/Signup';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Signup/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
