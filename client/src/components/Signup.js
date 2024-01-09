f6aa8d4c31d128bdaef8f35717941924be520a55
Jyothi-Gaddameedi <g.jyothi.779@gmail.com>
2024-01-09 15:56:34 +0530

    added

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -4,6 +4,10 @@ function Signup() {
   return (
     <div className='App'>
         <h2>Sign Up</h2>
+        <div>
+            <label>First Name</label>
+            <input type='name' placeholder='First Name'></input>
+        </div>
     </div>
   )
 }
